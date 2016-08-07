a42b2a0704b50834fa2fa121f5a80a4d2a4d7586
fletch <ste.fletcher@gmail.com>
2016-08-07 19:31:31 +0100

    initial commit

---
+++ src/test/groovy/BuildLogicFunctionTest.groovy
@@ -0,0 +1,35 @@
+import org.gradle.testkit.runner.GradleRunner
+import static org.gradle.testkit.runner.TaskOutcome.*
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Specification
+
+class BuildLogicFunctionalTest extends Specification {
+    @Rule final TemporaryFolder testProjectDir = new TemporaryFolder()
+    File buildFile
+
+    def setup() {
+        buildFile = testProjectDir.newFile('build.gradle')
+    }
+
+    def "hello world task prints hello world"() {
+        given:
+        buildFile << """
+            task helloWorld {
+                doLast {
+                    println 'Hello world!'
+                }
+            }
+        """
+
+        when:
+        def result = GradleRunner.create()
+                .withProjectDir(testProjectDir.root)
+                .withArguments('helloWorld')
+                .build()
+
+        then:
+        result.output.contains('Hello world!')
+        result.task(":helloWorld").outcome == SUCCESS
+    }
+}
